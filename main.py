501dc1ab81d9e9992499d47405428cf92b562821
mdv <devilliers.martin@gmail.com>
2026-06-11 12:27:20 +0100

    cache busting to make UI less sticky

--- main.py
+++ main.py
@@ -35,6 +35,14 @@ logger = logging.getLogger(__name__)
 
 app = FastAPI(title="Last Man Standing")
 
+@app.middleware("http")
+async def add_no_cache_header(request, call_next):
+    response = await call_next(request)
+    response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "0"
+    return response
+
 # OAuth2 context
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
